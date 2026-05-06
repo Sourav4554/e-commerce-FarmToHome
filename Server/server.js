6660bc5c468f522ee072f471b6aee502529f89ba
sourav <souravkrishna358@gmail.com>
2026-05-06 11:34:56 +0530

    mongodb function call

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,15 @@
 import app from "./app.js";
-import 'dotenv/config'
+import "dotenv/config";
 import { connectDatabase } from "./Config/mongodb.config.js";
 
-connectDatabase().then(() => {
-    // Only listen locally, Vercel handles it in production
-    if (process.env.NODE_ENV !== 'production') {
-        const PORT = process.env.PORT || 3000
-        app.listen(PORT, () => {
-            console.log(`http://localhost:${PORT}`)
-        })
-    }
-})
+connectDatabase();
+
+// Only listen locally, Vercel handles it in production
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+}
 
 export default app;
