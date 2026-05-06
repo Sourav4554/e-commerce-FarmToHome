ece371ed0d0b95e6e7d65d036e02889dc539dcf1
sourav <souravkrishna358@gmail.com>
2026-05-06 12:42:53 +0530

    fix logout button issue

--- Frontend/src/components/Logout.jsx
+++ Frontend/src/components/Logout.jsx
@@ -1,19 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import useLogin from "../hooks/authHook/useAuth";
 import toast from "react-hot-toast";
 import { AuthContextProvide } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 const Logout = () => {
   const navigate = useNavigate();
+  const [loader,setLoader]=useState(false)
   const {setUserInfo} = useContext(AuthContextProvide);
   const { logOut, error } = useLogin();
   //method for logout
   const handleLogout = async() => {
+    setLoader(true)
     const response = await logOut();
     if (!response.success) {
       toast.error(response.message);
+      setLoader(false)
       return
     }
+    setLoader(false)
     toast.success(response?.message);
     setUserInfo(null);
     navigate('/' ,{replace:true})
@@ -23,8 +27,9 @@ const Logout = () => {
       <button
         onClick={handleLogout}
         className="px-4 py-1.5 text-sm bg-red-600 text-white rounded-lg hover:bg-red-700 cursor-pointer"
+        disabled={loader}
       >
-        Logout
+        {loader?'Logout...':'Logout'}
       </button>
     </>
   );
